07298ca1623886875598052f9d273fa62bfb6c98
KenigGA <kenigsbergerga@gmail.com>
2017-10-03 02:53:08 +0300

    No ideas how to solve =(

--- task1/1.c
+++ task1/1.c
@@ -1,11 +1,9 @@
-#define 	CREATE_FIFO_ERROR 	-100
-#define		CANT_OPEN_FIFO_FOR_WR	-101
-#define 	CANT_OPEN_FILE		-102
+#define 	CREATE_FIFO_ERROR 		-100
+#define		CANT_OPEN_FIFO_FOR_WR		-101
+#define 	CANT_OPEN_FILE			-102
+#define 	INCORRECT_NUMBER_OF_ARGUMENTS	-103
 
-#define		OK 	0
-#define 	ERROR	-1
-
-#define BLOCK_SIZE 16000 // 16 kb
+#define BLOCK_SIZE 2 // 16 kb
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -15,13 +13,6 @@
 #include <errno.h>
 #include <malloc.h>
 
-int fifo_create(char* ffnm)
-{
-	if ((mknod(ffnm, S_IFIFO | 0666, 0) < 0) && (errno != EEXIST))
-        	return ERROR;
-	return OK;
-}
-
 int main(int argc, char** argv)
 {
 	int ffd, fld;
@@ -29,28 +20,45 @@ int main(int argc, char** argv)
 	size_t size;
 	char* mystr = (char*) calloc(BLOCK_SIZE, sizeof(char));
 	char name[] = "a.fifo";
+	struct stat sb;
 
 	umask(0);
-	if (res = fifo_create(name))
+	unlink("2");
+	if (argc != 2)
 	{
-		printf("CREATE_FIFO_ERROR");
+		printf("INCORRECT_NUMBER_OF_ARGUMENTS");
+		return INCORRECT_NUMBER_OF_ARGUMENTS;
+	}
+	if ((mknod(name, S_IFIFO | 0666, 0) < 0) && (errno != EEXIST))
+	{
+                printf("CREATE_FIFO_ERROR");
 		return CREATE_FIFO_ERROR;
 	}
+	errno = 0;
+/*
+	link(name, "a1.fifo");
+	if (errno == EEXIST)
+		return 0;
+*/
 	if ((fld = open(argv[1], O_RDONLY)) < 0) 
 	{
 		printf("CANT_OPEN_FILE");
 		return CANT_OPEN_FILE;
 	}
-	if ((ffd = open(name, O_WRONLY)) < 0)
+	if ((ffd = open(name, O_WRONLY | O_NONBLOCK)) < 0)
 	{
 		printf("CANT_OPEN_FIFO_FOR_WR");
+		printf("%d", errno);
+//		printf("%d", ENXIO);
 		return CANT_OPEN_FIFO_FOR_WR;
 	}
 	while (size = read(fld, mystr, BLOCK_SIZE))
-		size = write(ffd, mystr, BLOCK_SIZE); 
+		size = write(ffd, mystr, size);
 	close(ffd);
 	close(fld);
 	free(mystr);
+//	unlink("a1.fifo");
+	unlink("a.fifo");
 
 	return 0;
 }
--- task1/2.c
+++ task1/2.c
@@ -2,10 +2,7 @@
 #define         CANT_OPEN_FIFO_FOR_RD   -101
 #define 	CANT_OPEN_FILE		-102
 
-#define         OK      0
-#define         ERROR   -1
-
-#define BLOCK_SIZE 16000
+#define BLOCK_SIZE 2
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -15,41 +12,42 @@
 #include <errno.h>
 #include <malloc.h>
 
-int fifo_create(char* ffnm)
-{
-        if ((mknod(ffnm, S_IFIFO | 0666, 0) < 0) && (errno != EEXIST))
-                return ERROR;
-        return OK;
-}
-
 int main(int argc, char** argv)
 {
         int ffd, fld, res;
         size_t size;
         char* mystr = (char*) calloc(BLOCK_SIZE, sizeof(char));
         char name[] = "a.fifo";
+	char outnm[] = "2";
 
         umask(0);
-       	if (res = fifo_create(name))
-        {
-                printf("CREATE_FIFO_ERROR");
-                return CREATE_FIFO_ERROR;
-        }
+       	if ((mknod(name, S_IFIFO | 0666, 0) < 0) && (errno != EEXIST))
+	{
+		printf("CREATE_FIFO_ERROR");
+		return CREATE_FIFO_ERROR;	
+	}
+	errno = 0;
+/*
+	link(name, "a2.fifo");
+        if (errno == EEXIST)
+                return 0;
+*/
         if ((ffd = open(name, O_RDONLY)) < 0)
         {
                 printf("CANT_OPEN_FIFO_FOR_RD");
                 return CANT_OPEN_FIFO_FOR_RD;
         }
-	if ((fld = open(argv[1], O_WRONLY | O_CREAT, 0666)) < 0)
+	if ((fld = open(outnm, O_WRONLY | O_CREAT | O_APPEND, 0666)) < 0)
         {
                 printf("CANT_OPEN_FILE");
                 return CANT_OPEN_FILE;
         }
 	while (size = read(ffd, mystr, BLOCK_SIZE))
-		size = write(fld, mystr, BLOCK_SIZE);
+		size = write(fld, mystr, size);
         close(ffd);
 	close(fld);
 	free(mystr);
+	unlink("a2.fifo");
 
         return 0;
 }
